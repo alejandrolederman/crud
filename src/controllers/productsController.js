9daff6d3f2390ffc4613f79da17a00fe23b25441
alejandrolederman <alejandro.lederman@live.com.ar>
2021-11-24 12:26:36 -0300

    agregue funcionabilidad al detail.ejs

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,12 +1,10 @@
 const fs = require('fs');
 const path = require('path');
 
-
-
 const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
 const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 
-// const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+ const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 
 const controller = {
 	// Root - Show all products
@@ -18,18 +16,21 @@ const controller = {
 
 	// Detail - Detail from one product
 	detail: (req, res) => {
-		
-
-		// idPro = req.req.query.id;
-
-		
-
-		res.render ("detail");
-		// Do the magic
+		let elId = req.params.id;
+		let productos = products.find(unProducto => {
+			if (unProducto.id == elId){
+				return unProducto;
+			}
+			
+		});
+		res.render('detail',{ productos, products})
 	},
 
 	// Create - Form to create
 	create: (req, res) => {
+
+		res.render('product-create-form',{category});
+	
 		// Do the magic
 	},
 	
@@ -49,8 +50,90 @@ const controller = {
 
 	// Delete - Delete one product from DB
 	destroy : (req, res) => {
+		let idProducto = req.params.id;
+		products = products.filter(unProducto => { 
+		 return unProducto.id != idProducto;
+		
+		})
+			  res.send(products);
+		
+
 		// Do the magic
 	}
 };
 
 module.exports = controller;
+
+
+
+
+
+
+//     // Add - Cart
+// 	cart: (req, res) => {
+// 		res.render('productCart',{category});
+// 	},
+	
+// 	// Create -  Method to store
+// 	store: (req, res) => {
+// 		const {name, price, discount, category, description} = req.body;
+// 		const lastIndex = products.length - 1;
+// 		const id = products[lastIndex].id + 1;
+// 		const val = {	'id':id,
+// 						'name':name,
+// 						'price':price,
+// 						'image':'img-bicicleta-fierce.jpg',
+// 						'discount':discount,
+// 						'category':category,
+// 						'description':description};
+// 		products.push(val);
+// 		fs.writeFileSync(productsFilePath,JSON.stringify(products),'utf-8');
+// 		res.redirect('/products');
+// 	},
+
+// 	// Update - Form to edit
+// 	edit: (req, res) => {
+// 		const productToEdit = products.find(val => {
+// 			if (val.id == req.params.id){
+// 				return val;
+// 			}
+// 		})
+// 		res.render('product-edit-form',{productToEdit,category});
+// 	},
+// 	// Update - Method to update
+// 	update: (req, res) => {
+// 		const {name, price, discount, category, description} = req.body;
+// 		const id = req.params.id;
+// 		const newProducts = [];
+// 		products.map(val=>{
+// 			if (val.id == id){
+// 				val.name=name;
+// 				val.price=price;
+// 				val.discount=discount;
+// 				val.category=category;
+// 				val.description=description;
+// 				newProducts.push(val);
+// 			} else {
+// 				newProducts.push(val);
+// 			}
+// 		})
+// 		fs.writeFileSync(productsFilePath,JSON.stringify(newProducts),'utf-8');
+// 		res.redirect('detail/' + id);
+// 	},
+
+// 	// Delete - Delete one product from DB
+// 	destroy : (req, res) => {
+// 		const id = req.params.id;
+// 		const newProducts = [];
+// 		products.map(val => {
+// 			if (val.id != id){
+// 				newProducts.push(val);
+// 			}
+// 		});
+		
+// 		fs.writeFileSync(productsFilePath,JSON.stringify(newProducts),'utf-8');
+// 		res.redirect('/products');
+// 	}
+// };
+
+// module.exports = controller;
